ce53e1432793cdf88ed53bd29874c317b9a57645
Leandro Jesus <ajleandroaj@gmail.com>
2022-03-08 14:51:28 +0000

    chore(codecademy): add description to Challenge classes

--- codecademy/src/main/java/Challenge1.java
+++ codecademy/src/main/java/Challenge1.java
@@ -1,13 +1,29 @@
+import java.awt.datatransfer.StringSelection;
+import java.util.Comparator;
+
+/**
+ * Description:
+ * 1. Write a Java program to reverse a string without using the reverse method of Java's String class
+ * This exercise will teach you about how Java handles strings. First, you should create a class with a main method that accepts a string and then returns a string where the characters are in reverse order. But, you can't use the reverse method of the String class because that’d be cheating.
+ */
 public class Challenge1 {
 
     static String reverseString(String toReverse) {
         StringBuilder result = new StringBuilder();
-        for (char character: toReverse.toCharArray()) {
+        for (char character : toReverse.toCharArray()) {
             result.insert(0, character);
         }
 
         return result.toString();
     }
+
+    /* Cheating way
+    static String reverseString(String toReverse) {
+        return new StringBuilder(toReverse)
+                .reverse()
+                .toString();
+    }
+    */
 
     /* Pig way
     static String reverseString(String toReverse){
--- codecademy/src/main/java/Challenge2.java
+++ codecademy/src/main/java/Challenge2.java
@@ -1,8 +1,15 @@
 import java.util.Arrays;
 
+/**
+ * Description:
+ * 2. Write a Java program to determine whether a number is prime or not
+ *    This program should accept an array of numbers. It should iterate through the array of numbers and determine if each is prime or not. Once it’s done processing, it should return only those numbers that are prime in an array.
+ */
 public class Challenge2 {
     static Integer[] filterByPrimeNumbers(Integer[] numbers) {
-        return Arrays.stream(numbers).filter(Challenge2::isPrime).toArray(Integer[]::new);
+        return Arrays.stream(numbers)
+                .filter(Challenge2::isPrime)
+                .toArray(Integer[]::new);
     }
 
     static boolean isPrime(int number) {
